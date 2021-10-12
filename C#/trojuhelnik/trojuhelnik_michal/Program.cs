c561df224ad6a3e0035dd5560732a1b675033474
Michael <49836430+ZirixCZ@users.noreply.github.com>
2021-10-12 22:13:15 +0200

    očísloval jsem pro jednoduché orientování

--- C#/trojuhelnik/trojuhelnik_michal/Program.cs
+++ C#/trojuhelnik/trojuhelnik_michal/Program.cs
@@ -6,45 +6,45 @@ namespace prj_trojuhelnik
     {
         static void Main(string[] args)
         {
-            // Promněnné musí být deklarovány zvenčí do,while cyklu, aby se dali použít na více místech v kódu.
+            // 1. Promněnné musí být deklarovány zvenčí do,while cyklu, aby se dali použít na více místech v kódu.
             bool checker1, checker2, checker3;
             double strana1, strana2, strana3;
-            // Všechen kód běží v jednom while true cyklu. tímto způsobem uživatel nebude nucen program zavírat a znovu otevírat pro další použití.
+            // 2. Všechen kód běží v jednom while true cyklu. tímto způsobem uživatel nebude nucen program zavírat a znovu otevírat pro další použití.
             while (true)
             {
                 // Opakuj kód v do{}, dokud bude podmínka pravdivá
                 do
                 {
                     Console.WriteLine("\nNapiste tri cisla, prosim.");
-                    checker1 = double.TryParse(Console.ReadLine(), out strana1); // Bere si promněnné jako string a konvertuje je do typu double. Toto celé je kontrolováno boolem (Kontroluje, zda je TryParse validní, tedy jestli uživatel zadal číslo).
+                    checker1 = double.TryParse(Console.ReadLine(), out strana1); // 3. Bere si promněnné jako string a konvertuje je do typu double. Toto celé je kontrolováno boolem (Kontroluje, zda je TryParse validní, tedy jestli uživatel zadal číslo).
                     checker2 = double.TryParse(Console.ReadLine(), out strana2);
                     checker3 = double.TryParse(Console.ReadLine(), out strana3);
-                } while (!checker1 || !checker2 || !checker3); // Jestli uživatel nezadal číslo u jednoho, či více promněnných, do,while cykl bude pravdivý a tak se bude opakovat do té doby, dokud uživatel zadá čísla. (a podmínka bude lživá).
+                } while (!checker1 || !checker2 || !checker3); // 4. Jestli uživatel nezadal číslo u jednoho, či více promněnných, do,while cykl bude pravdivý a tak se bude opakovat do té doby, dokud uživatel zadá čísla. (a podmínka bude lživá).
             
 
-            // Zjistí, zda je součet dvou stran vždy větší, než strana třetí. tímto způsobem si ověříme, jestli uživatel zapsal validní velikosti stran trojúhelníku a má smysl v programu pokračovat.
+            // 5. Zjistí, zda je součet dvou stran vždy větší, než strana třetí. tímto způsobem si ověříme, jestli uživatel zapsal validní velikosti stran trojúhelníku a má smysl v programu pokračovat.
             if (strana1 + strana3 > strana2 && strana1 + strana2 > strana3 && strana2 + strana3 > strana1)
                 {
                     Console.WriteLine("{0}, {1}, {2} jsou stranami trojuhelniku.", strana1, strana2, strana3);
-                    // Jestli se všechny délky stran rovnají, je to trojúhelník rovnostranný. jestli (a = b = c)
+                    // 6. Jestli se všechny délky stran rovnají, je to trojúhelník rovnostranný. jestli (a = b = c)
                     if (strana1 == strana2 && strana2 == strana3)
                         Console.WriteLine("trojuhelnik je rovnostranny");
-                    // Jestli alespoň dvě zadané strany jsou stejně dlouhé, jedná se o trojúhelník rovnoramenný. jestli (a = b, nebo b = c)
+                    // 7. Jestli alespoň dvě zadané strany jsou stejně dlouhé, jedná se o trojúhelník rovnoramenný. jestli (a = b, nebo b = c)
                     else if (strana1 == strana2 || strana2 == strana3 || strana1 == strana3)
                         Console.WriteLine("trojuhelnik je rovnoramenny");
-                    // Vytvoříme si pole double o velikosti 3 (počínaje od 0 do 3) poté do každého volného místa (od nuly do tří) přidám inviduální stranu.
+                    // 8. Vytvoříme si pole double o velikosti 3 (počínaje od 0 do 3) poté do každého volného místa (od nuly do tří) přidám inviduální stranu.
                     double[] arrDouble = new double[3];
                     arrDouble[0] = strana1*strana1;
                     arrDouble[1] = strana2*strana2;
                     arrDouble[2] = strana3*strana3;
-                    // Použiji metodu Array.Sort, která v mém případě využívá "Insertion Sort" algortimus pro seřazení čísel v poli od nejmenšího po největší. více informací pod kódem v *insertion sort
+                    // 9. Použiji metodu Array.Sort, která v mém případě využívá "Insertion Sort" algortimus pro seřazení čísel v poli od nejmenšího po největší. více informací pod kódem v *insertion sort
                     Array.Sort(arrDouble);
                     
-                    // Pythagorova veta = jednoZMensichCisel + jednoZMensichCisel = NejvetsiCislo (vše na druhou). na pozici 2 (na poslední pozici) máme díky array.sort logicky největší číslo a tak ho dosadíme doprava.
+                    // 10. Pythagorova veta = jednoZMensichCisel + jednoZMensichCisel = NejvetsiCislo (vše na druhou). na pozici 2 (na poslední pozici) máme díky array.sort logicky největší číslo a tak ho dosadíme doprava.
                     if (arrDouble[0] + arrDouble[1] == arrDouble[2])
                         Console.WriteLine("trojuhelnik je pravouhly");
                 }
-                // pokud uživatel zadá nevalidní velikosti stran a tedy se o trojúhelník nejedná,
+                // 11. pokud uživatel zadá nevalidní velikosti stran a tedy se o trojúhelník nejedná,
                 // program se ukončuje (v tomto případě začíná od začátku, jelikož celý kód běží ve while true cyklu)
                 else
                     Console.WriteLine("zadane delky stran nemohou byti delkami stran trojuhelniku..");
@@ -53,6 +53,6 @@ namespace prj_trojuhelnik
     }
 }
 
-// to, který algorithmus array.sort použije záleží na velikosti danného pole. 
+// 12. to, který algorithmus array.sort použije záleží na velikosti danného pole. 
 // pro pole s velikostí menší, než 16, se použije mnou zmiňovaný insertion sort. Jde o algorithmus, který si vezme číslo v poli "n" a číslo v poli "n+1" a porovná, které je větší. větší posune doprava. (n plus 1).
 // Poté se také používá QuickSort a HeapSort.
