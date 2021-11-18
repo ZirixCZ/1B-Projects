deacaf4a09402a46a59756a8d578f3f3caaf37ed
LukasBrychta <92089539+LukasBrychta@users.noreply.github.com>
2021-11-18 12:31:00 +0100

    Add files via upload

---
+++ C#/cyklo_priklady/Lukas/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rovnice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string odpoved;
+            int uzivatel;
+            bool vysledek;
+            int spravne = 0;
+            int spatne = 0;
+            string operace;
+            Random generator = new Random();
+
+            
+           while (true)
+            {
+                Console.WriteLine("Jaký typ příkladů chcete? +, -, *");
+                operace = Console.ReadLine();
+
+                if (operace == "*")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        int cisloA = generator.Next(1, 11);
+                        int cisloB = generator.Next(1, 11);
+                        Console.WriteLine("Jaký je výsledek rovnice " + cisloA + " * " + cisloB);
+                        //uzivatel = int.Parse(Console.ReadLine());
+
+                        while (!int.TryParse(Console.ReadLine(), out uzivatel))
+                        {
+                            Console.WriteLine("Musíš zadat číslo");
+                        }
+                        vysledek = (cisloA * cisloB) == uzivatel;
+
+                        if (vysledek)
+                        {
+                            Console.WriteLine("Správná odpověď");
+                            spravne++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Chybná odpověď");
+                            spatne++;
+                        }
+                        Console.WriteLine("Chceš pokračovat? a/n");
+                        odpoved = Console.ReadLine().ToUpper();
+                    }
+                    while (odpoved == "A");
+                }
+                else if (operace == "+")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        int cisloA = generator.Next(1, 11);
+                        int cisloB = generator.Next(1, 11);
+                        Console.WriteLine("Jaký je výsledek rovnice " + cisloA + " + " + cisloB);
+                        //uzivatel = int.Parse(Console.ReadLine());
+
+                        while (!int.TryParse(Console.ReadLine(), out uzivatel))
+                        {
+                            Console.WriteLine("Musíš zadat číslo");
+                        }
+                        vysledek = (cisloA + cisloB) == uzivatel;
+
+                        if (vysledek)
+                        {
+                            Console.WriteLine("Správná odpověď");
+                            spravne++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Chybná odpověď");
+                            spatne++;
+                        }
+                        Console.WriteLine("Chceš pokračovat? a/n");
+                        odpoved = Console.ReadLine().ToUpper();
+                    }
+                    while (odpoved == "A");
+                }
+                else if (operace == "-")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        int cisloA = generator.Next(1, 11);
+                        int cisloB = generator.Next(1, 11);
+                        Console.WriteLine("Jaký je výsledek rovnice " + cisloA + " - " + cisloB);
+                        //uzivatel = int.Parse(Console.ReadLine());
+
+                        while (!int.TryParse(Console.ReadLine(), out uzivatel))
+                        {
+                            Console.WriteLine("Musíš zadat číslo");
+                        }
+                        vysledek = (cisloA - cisloB) == uzivatel;
+
+                        if (vysledek)
+                        {
+                            Console.WriteLine("Správná odpověď");
+                            spravne++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Chybná odpověď");
+                            spatne++;
+                        }
+                        Console.WriteLine("Chceš pokračovat? a/n");
+                        odpoved = Console.ReadLine().ToUpper();
+                    }
+                    while (odpoved == "A");
+                }
+                Console.WriteLine("Počet správných odpovědí: " + spravne + " počet chybných odpovědí: " + spatne);
+
+            }
+                
+
+            Console.ReadKey();
+        }
+    }
+}
